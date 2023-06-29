312bdd173935d05900f86d08300c90ffa1a8c40e
Brian Wo <45139213+brainwo@users.noreply.github.com>
2023-06-30 04:11:49 +0800

    docs: add and clean up some long comments

--- lib/model/setting_options.dart
+++ lib/model/setting_options.dart
@@ -4,6 +4,12 @@ abstract class SettingOptions {
   int currentIndex();
 }
 
+/// Application behavior when a content is played.
+///
+/// `nothing`: do nothing
+/// `minimize`: app window is minimized, may not work on some desktop environment
+/// `tray`: close app window and leave the system tray available
+/// `exit`: kill app entirely, will not leave the system tray
 enum OnPlayOptions implements SettingOptions {
   nothing,
   minimize,
@@ -22,6 +28,12 @@ enum OnPlayOptions implements SettingOptions {
   int currentIndex() => super.index;
 }
 
+/// Theme brightness mode.
+///
+/// `light`: set app to light mode immediately
+/// `dark`: set app to dark mode immediately
+/// `system`: set app to system light/dark mode immediately, currently there is
+///         no implementation available for Linux in Flutter's engine
 enum BrightnessOptions implements SettingOptions {
   light,
   dark,
@@ -39,6 +51,12 @@ enum BrightnessOptions implements SettingOptions {
   int currentIndex() => super.index;
 }
 
+/// Visual desity mode controls how dense the UI looks. The densier the UI is,
+/// the more information can present in one screen. The less densier the UI is,
+/// the more whitespaces present in the screen, making it more comfortable to
+/// look at.
+///
+/// TODO
 enum VisualDensityOptions implements SettingOptions {
   compact,
   standard,
