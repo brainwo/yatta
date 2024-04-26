11dd682ed73a5698d7cdaa21f58de46bb9769b91
Brian Wo <45139213+brainwo@users.noreply.github.com>
2024-04-26 15:12:42 +0800

    refactor: previous refactor

--- lib/widget/list_items/list_item.dart
+++ lib/widget/list_items/list_item.dart
@@ -7,6 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:youtube_api/youtube_api.dart';
 import '../../helper/command_parser.dart';
+import '../../helper/time.dart';
 import '../../intent.dart';
 import '../../locale/en_us.dart';
 import '../keyboard_navigation.dart';
